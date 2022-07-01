602d4638d68d1f131595b0fab6f42b30de9aa47b
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-07-01 23:41:30 +0900

    header 추가

---
+++ src/component/header/header.js
@@ -0,0 +1,13 @@
+
+function App() {
+    return (
+    <header>
+        <nav>
+            <a href="#">Home</a>
+            <a th:href="/member/login">Log In</a>
+            <a href="#">Profile</a>
+            <a href="#">Programs</a>
+        </nav>
+    </header>
+    );
+}
