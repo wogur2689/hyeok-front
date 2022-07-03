373bef384264d8c7fde4ebad9e2cce100f025ad0
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-07-04 00:14:42 +0900

    회원가입시 주소, 전화번호 추가

--- src/component/content/member/signup.js
+++ src/component/content/member/signup.js
@@ -21,16 +21,23 @@ function SignUp() {
     const [userId, setUserId] = useState("");
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
+    const [address, setAddress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
     const onChangeUserId = (e) => {
-        setUserId(e.target.value);
+      setUserId(e.target.value);
     };
     const onChangeName = (e) => {
       setName(e.target.value);
     };
     const onChangePassword = (e) => {
-        setPassword(e.target.value);
+      setPassword(e.target.value);
     };
-
+    const onChangeAddress = (e) => {
+      setAddress(e.target.value);
+    }
+    const onChangePhoneNumber = (e) => {
+      setPhoneNumber(e.target.value);
+    }
     return(
         <section>
             <h1>회원가입</h1>
@@ -38,6 +45,8 @@ function SignUp() {
                 <input type="text" name="userId" placeholder="아이디" onChangeUserId={onChangeUserId} value={userId}/>
                 <input type="text" name="name" placeholder="이름" ohnChangeName={onChangeName} value={name}/>
                 <input type="password" name="password" placeholder="비밀번호" onChangePassword={onChangePassword} value={password}/>
+                <input type="text" name="address" placeholder="주소" ohnChangeAddress={onChangeAddress} value={address}/>
+                <input type="text" name="phoneNumber" placeholder="전화번호" ohnChangeName={onChangePhoneNumber} value={phoneNumber}/>
                 <input type="submit" value="가입" onClick={signup}/>
             </form>
         </section>
