ad9c366496a1dd69e0d3bfca870509639358178a
jaehyeokGil <81075170+wogur2689@users.noreply.github.com>
2022-07-14 18:01:15 +0900

    헤더 디자인

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,14 +1,16 @@
+import styles from './header.module.css';
+
 function Header() {
     return (
-    <header>
-        <h1>로고 및 헤더</h1>
-        <nav>
-            <a href="#">Home</a>
-            <a href="/member/login">Log In</a>
-            <a href="#">Profile</a>
-            <a href="#">Programs</a>
-        </nav>
-    </header>
+        <header className={styles.header}>
+            로고 및 헤더
+            <nav className={styles.nav}>
+                <div className={styles.div}><a href="#" className={styles.a}>Home</a></div>
+                <div className={styles.div}><a href="#" className={styles.a}>Sign In</a></div>
+                <div className={styles.div}><a href="#" className={styles.a}>Board</a></div>
+                <div className={styles.div}><a href="#" className={styles.a}>Map</a></div>
+            </nav>
+        </header>
     );
 }
 
