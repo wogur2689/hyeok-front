61e23cfcb402e2aa6e5c8b5fdc6f0acc874ab6fa
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2023-04-26 13:48:26 +0900

    [refactor] 코드정리

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,18 +1,20 @@
 import styles from './footer.module.css';
 
+export function Links(props) {
+    return (
+        <a href={props.url} target='_blank' rel='noreferrer'>
+            <img src={props.img} alt={props.alt} className={styles.fooIcon}/>
+        </a>
+    );
+}
+
 function Footer() {
     return (
         <footer className={styles.foo}>
             <div className={styles.imgs}>
-                <a href="https://github.com" target='_blank' rel='noreferrer'>
-                    <img src="img/github.png" alt="github" className={styles.fooIcon}/>
-                </a>
-                <a href='https://www.instagram.com' target='_blank' rel='noreferrer'>
-                    <img src="img/instagram.png" alt="instagram" className={styles.fooIcon}/>
-                </a>
-                <a href='https://discord.com' target='_blank' rel='noreferrer'>
-                    <img src="img/discord.png" alt="discord" className={styles.fooIcon}/>
-                </a>
+                <Links url={"https://github.com"} img={"img/github.png"} alt={"github"} />
+                <Links url={"https://www.instagram.com"} img={"img/instagram.png"} alt={"instagram"}/>
+                <Links url={"https://discord.com"} img={"img/discord.png"} alt={"discord"}/>
             </div>
         </footer>
     );
